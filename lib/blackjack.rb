01f25aff3085e79de43135b0e0f981f135674bc9
Dan Powell <powell956@gmail.com>
2019-01-05 22:01:18 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -50,5 +50,11 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?
+    display_card_total(card_total)
+  end
+  end_game
 end
